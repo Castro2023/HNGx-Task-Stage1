673e1d5b0e59fe6cf16532185acf67eea95996ba
charles <charlesugwoke93@gmail.com>
2023-09-09 16:37:31 +0100

    data-testid implemented

--- stage1.js
+++ stage1.js
@@ -1,16 +1,15 @@
 currentDayOfWeek=()=> {
-    const dayOfWeekElement = document.getElementById("currentDayOfTheWeek");
+    const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const now = new Date();
     const dayOfWeek = daysOfWeek[now.getDay()];
 
     dayOfWeekElement.textContent = `Current Day of the Week: ${dayOfWeek}`;
-}
+};
 currentDayOfWeek();
-
 currentTimeMillis=()=> {
-    const millisTimeElement = document.getElementById("currentUTCTime");
+    const millisTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
     const currentTimeMillis = new Date().getTime();
     millisTimeElement.textContent = `Current UTC Time: ${currentTimeMillis}`;
-}
+};
 currentTimeMillis();
